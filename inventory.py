abca41223b459e4f219b9a571a1500cf44eacac2
Chris Brinkley <brinklin8r@outlook.com>
2022-06-26 16:38:16 -0700

    Updated inventory.py to use classes. Created rpg.py to be a test case / switchboard

--- inventory.py
+++ inventory.py
@@ -1,168 +1,139 @@
 #! python3
 # inventory.py
-# RPG inventory script
-from pathlib import Path
-import pprint
-import sys
-import importlib
+# RPG CRUD inventory management script v2
 
-### Functions:
-def displayItem(item, colWidths):
-    print(item['name'].ljust(colWidths[0]),                                 \
-          str(item['count']).rjust(colWidths[1]),                           \
-          "{:.2f}".format(item['weight']).rjust(colWidths[2]),              \
-          "${:.2f}".format(item['price']).rjust(colWidths[3]),              \
-          sep=' | ', end= ' |\n')
+from ast import Not
+from tkinter import W
 
-def displayInventory(inventory):
-    itemsWeight = 0.0
-    itemsPrice = 0.00
-    itemsCount = 0
-    colWidths = [0] * len(inventory[0])         # List for Number of Columns
-    totalWidth = 0
 
-    for item in inventory:                      # Calculate column widths
-        if colWidths[0]  < len(item['name']):
-            colWidths[0] = len(item['name'])
-        if colWidths[1]  < len(str(item['count'])):
-            colWidths[1] = len(str(item['count']))
-        if colWidths[2]  < len(str(item['weight'])):
-            colWidths[2] = len(str(item['weight']))
-        if colWidths[3]  < len(str(item['price'])) + 2:
-            colWidths[3] = len(str(item['price'])) + 2
-    
-    for length in colWidths:                    # Calculate Width of Inventory Report
-        totalWidth += length + 3                # Adjust for spacer character " | "
-    totalWidth -= 1                             # Adjust for last character being " |"
-    
-    # Inventory Report Header
-    print('-' * totalWidth)
-    print('Item Name'.center(colWidths[0]), 'Count'.center(colWidths[1]),   \
-          'Weight'.center(colWidths[2]),    'Price'.center(colWidths[3]),   \
-          sep=' | ', end= ' |\n')
-    print('-' * colWidths[0], '-' * colWidths[1],                           \
-          '-' * colWidths[2], '-' * colWidths[3],                           \
-          sep='-+-', end= '-|\n')
+class Slot():
+    '''Simple inventory slot model.'''
+    def __init__(self, itemName, itemCount = 1, itemWeight = 0.0, itemPrice = 0.0 ):
+        self.itemName   = itemName 
+        self.itemCount  = itemCount
+        self.itemWeight = itemWeight
+        self.itemPrice  = itemPrice
 
-    # Inventory Report Item(s)
-    for itemNum in range(1, len(inventory)):
-        displayItem(inventory[itemNum], colWidths)
-        itemsWeight += inventory[itemNum]['weight'] * inventory[itemNum]['count']
-        itemsPrice  += inventory[itemNum]['price']  * inventory[itemNum]['count']
-        itemsCount  += inventory[itemNum]['count']
+    def displayItem(self, columnWidths):
+        '''Displays a single inventory item.'''
+        print(self.itemName.ljust(columnWidths[0]),                            \
+              str(self.itemCount).rjust(columnWidths[1]),                      \
+              "{:.2f}".format(self.itemWeight).rjust(columnWidths[2]),         \
+              "${:.2f}".format(self.itemPrice).rjust(columnWidths[3]),         \
+              sep=' | ', end= ' |\n')
 
-    # Inventory Report Footer
-    print('=' * totalWidth)
-    print('Total Items:'.ljust(13),  str(itemsCount).rjust(10))
-    print('Total Weight:'.ljust(13), "{:.2f}".format(itemsWeight).rjust(10))
-    print('Total Value:'.ljust(13),  "${:.2f}".format(itemsPrice).rjust(10))
+################################################################################
+class Inventory():
+    '''Simple inventory management model.'''
+    def __init__(self):
+        self.inventory = []
 
-def saveInventory(inventory, saveFileName):
-    saveFile = open(saveFileName, 'w')
-    saveFile.write('inventory = ' + pprint.pformat(inventory) + '\n')
-    saveFile.close()
-    print(f'Inventory saved to {saveFileName}.')
+    # Add item to inventory.
+    def addItem(self, item):
+        '''Adds an item to the inventory.'''
+        # Check if there is already an item with the same name.
+        found = False
+        for slot in self.inventory:
+            # Yes: update count
+            if slot.itemName == item.itemName:
+                slot.itemCount += item.itemCount
+                found = True
+                break
+        if not(found):
+            # No: add item
+            self.inventory.append(item)
 
-def addItem(inventory, addItem):
-    found = False
-    for item in inventory:                      # Check if there is already an item by this name in inventory
-        if addItem['name'] == item['name']:     # Increment count if found
-            item['count'] += addItem['count']
-            print(f"Found item {addItem['name']}, incremented count to {item['count']}.")
-            found = True
-            break
-    if not found:                               # Add item if not already in inventory
-        inventory.append(addItem)
-        print(f"Added item {addItem['name']} to inventory.")
+    # Remove item from inventory.
+    def deleteItem(self, item, delCount = 1):
+        '''Removes an item to the inventory.'''
+        # Check if there is an item with the same name.
+        found = False
+        slotCount = 0
+        for slot in self.inventory:
+            # Yes: update count if there are enough.
+            if slot.itemName == item.itemName:
+                if slot.itemCount > delCount:
+                    slot.itemCount -= delCount
+                    print(f'Removed {delCount} of {item.itemName}s from your inventory.')
+                elif delCount == slot.itemCount:
+                    del self.inventory[slotCount]
+                    print(f'Removed all {item.itemName}s from your inventory.')
+                else:
+                    # ERROR if not enough.
+                    print(f'Unable to delete {delCount} {item.itemName}s.  You only have {slot.itemCount}!')
+                found = True
+                break
+            slotCount += 1
+        if not(found):
+            # No: ERROR
+            print(f'Unable to delete {item.itemName}s.  You have none!')
 
-def removeItem(inventory, removeItem):
-    found = False
-    for item in inventory:                      # Check if there is an item by this name in inventory
-        if removeItem['name'] == item['name']:  # Decrement count if found, and there are enough
-            if item['count'] > removeItem['count']:
-                item['count'] -= removeItem['count']
-                print(f"Found item {removeItem['name']}, decremented count to {item['count']}.")
-            elif item['count'] == removeItem['count']:
-                inventory.remove(item)          # Remove item from bag if remove count is inventory count
-                print(f"Found item {removeItem['name']}, removed all from inventory.")
-            else:
-                print(f"Found item {removeItem['name']}, but unable to decrement count as there are only {item['count']} in inventory.")
-            found = True
-            break
-    if not found:                               # Item is not in inventory
-        print(f"Item {removeItem['name']} not found in inventory.")
 
-def updateItem(inventory, item):
-    print('Update Item')
+    # Update item in inventory.
+    def updateItem(self, item):
+        '''Updates an item's info in the inventory.'''
+        # Check if there is already an item with the same name.
+        found = False
+        for slot in self.inventory:
+            # Yes: update info
+            if slot.itemName == item.itemName:
+                slot.itemCount = item.itemCount
+                slot.itemWeight = item.itemWeight
+                slot.itemPrice = item.itemPrice
+                print(f'Updated {item.itemName} with new values.')
+                found = True
+                break
+        if not(found):
+            # No: ERROR
+            print(f'Unable to find item {item.itemName}.')
 
-def getItem(forAddRemoveUpdate = ''):
-    item = {'name'  : '', 
-            'count' : 0, 
-            'weight': 0.0, 
-            'price' : 0.00}
+    # Display inventory.
+    def displayInventory(self):
+        '''Displays the inventory report including total weight, total item count
+            and total item price.'''
+        # for item in self.inventory:
+        #     print(item.itemName)
+        #     print(item.itemCount)
+        #     print(item.itemWeight)
+        #     print(item.itemPrice)
+        itemsWeight = 0.0
+        itemsPrice = 0.00
+        itemsCount = 0
+        colWidths = [9, 5, 6, 5]                         # List of length of column headers.
+        totalWidth = 0
 
-    # Get Name from User
-    # Find Item 
+        for item in self.inventory:                      # Calculate column widths
+            if colWidths[0]  < len(item.itemName):
+                colWidths[0] = len(item.itemName)
+            if colWidths[1]  < len(str(item.itemCount)):
+                colWidths[1] = len(str(item.itemCount))
+            if colWidths[2]  < len(str(item.itemWeight)):
+                colWidths[2] = len(str(item.itemWeight))
+            if colWidths[3]  < len(str(item.itemPrice)) + 2:
+                colWidths[3] = len(str(item.itemPrice)) + 2
 
-    return item
+        for length in colWidths:                        # Calculate Width of Inventory Report
+            totalWidth += length + 3                    # Adjust for spacer character " | "
+        totalWidth -= 1                                 # Adjust for last character being " |"
 
-### Variables:
-bag = []
-saveFile = 'inventorySave.py'
+        # Inventory Report Header
+        print('-' * totalWidth)
+        print('Item Name'.center(colWidths[0]), 'Count'.center(colWidths[1]),  \
+                'Weight'.center(colWidths[2]),    'Price'.center(colWidths[3]),\
+                sep=' | ', end= ' |\n')
+        print('-' * colWidths[0], '-' * colWidths[1],                          \
+                '-' * colWidths[2], '-' * colWidths[3],                        \
+                sep='-+-', end= '-|\n')
 
-### Main:
-if Path(saveFile).is_file():                    # Load Saved Inventory
-    savedInventory = importlib.import_module(Path(saveFile).stem)
-    print(f'Reading Inventory from {saveFile}.')
-    bag = savedInventory.inventory
+        # Inventory Report Item(s)
+        for item in self.inventory:
+            item.displayItem(colWidths)
+            itemsWeight += item.itemWeight * item.itemCount
+            itemsPrice  += item.itemPrice  * item.itemCount
+            itemsCount  += item.itemCount
 
-if len(bag) == 0:
-    # Initialize Inventory and Create Header Row if inventory is empty
-    bag = [{'name'  : 'Item Name', 
-            'count' : 'Count', 
-            'weight': 'Weight', 
-            'price' : 'Price'}]
-
-while True:                                     # Main program
-    # Switchboard
-    while True:                                 # Get valid user input
-        print(' RPG Inventory System '.center(38, '='))
-        print('  d) Display Inventory')
-        print('  a) Add Item(s) to Inventory')
-        print('  r) Remove Item(s) from Inventory')
-        print('  u) Update Item')
-        print()
-        print('  x) Save and Exit')
-        print('\n> ', end='')
-        choice = input()
-        if choice == 'x':
-            # Save Inventory and EXIT
-            saveInventory(bag, saveFile)
-            sys.exit()
-        if choice == 'd' or choice == 'a' or choice == 'r' or choice == 'u':
-            break                               # Process user input
-
-    if choice == 'a':
-        # Add Item to Inventory
-        addItem(bag, {'name'  : 'Paper', 
-                      'count' : 2,
-                      'price' : .50, 
-                      'weight': .75})
-
-    if choice == 'r':
-        # Remove Item(s) from Inventory
-        removeItem(bag, {'name' : 'Gold', 
-                        'count' : 1,
-                        'price' : 1.0, 
-                        'weight': 1})
-
-    if choice == 'd':
-        # Display Inventory
-        displayInventory(bag)
-
-    if choice == 'u':
-        # Update Item to Inventory
-        updateItem(bag, {'name'  : 'Rock', 
-                         'count' : 2,
-                         'price' : .7, 
-                         'weight': 6.0})
+        # Inventory Report Footer
+        print('=' * totalWidth)
+        print('Total Items:'.ljust(13),  str(itemsCount).rjust(10))
+        print('Total Weight:'.ljust(13), "{:.2f}".format(itemsWeight).rjust(10))
+        print('Total Value:'.ljust(13),  "${:.2f}".format(itemsPrice).rjust(10))
---
+++ rpg.py
@@ -0,0 +1,34 @@
+#! python3
+# rpg.py
+# RPG script
+from inventory import Slot, Inventory
+
+item1 = Slot('Default Knife')
+item2 = Slot('Full Item', 5, 4.0, 1.10)
+item3 = Slot('To be deleted', 3, 1.0, 7.15)
+item4 = Slot('fourth item', 1, 1, 2)
+dupeItem = Slot('Default Knife', 2, 10)
+noItem = Slot('Not in Inventory')
+
+bag = Inventory()
+
+bag.addItem(item1)
+bag.addItem(item2)
+bag.addItem(item3)
+bag.addItem(item4)
+bag.addItem(dupeItem)
+bag.displayInventory()
+
+bag.deleteItem(item1)
+bag.displayInventory()
+bag.deleteItem(noItem)
+bag.displayInventory()
+bag.deleteItem(item3, 3)
+bag.displayInventory()
+bag.deleteItem(item2, 100)
+bag.displayInventory()
+
+bag.updateItem(Slot('Default Knife', 6, 1.9, 20.5))
+bag.displayInventory()
+bag.updateItem(noItem)
+bag.displayInventory()
